6d78e24627a4633940a9e2ed58cf35d206af2d35
An at home workstation <hostmaster@truongan.name.vn>
2021-05-15 14:03:46 +0700

    working on line compononent

--- App.js
+++ App.js
@@ -55,6 +55,39 @@ const Section = ({children, title}): Node => {
   );
 };
 
+function calc(previous, current){
+  return current - previous;
+}
+
+const Line = (props) =>{
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View 
+      style={styles.line}
+      
+    >
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+            paddingHorizontal: 24,
+          },
+        ]}>
+        {props.current}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {calc(props.previous, props.current)}
+      </Text>
+    </View>
+  );
+};
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -76,15 +109,9 @@ const App: () => Node = () => {
           }}>
           <Section title="16/04/2021">
           </Section>
-          <View style={[styles.sectionContainer, {backgroundColor: "white"}]}>
-            <Text>abc</Text>
-          </View>
-          <View style={[styles.sectionContainer, {backgroundColor: "white"}]}>
-            <Text>abc</Text>
-          </View>
-          <View style={[styles.sectionContainer, {backgroundColor: "white"}]}>
-            <Text>abc</Text>
-          </View>
+
+          <Line previous='1' current='2'/>
+          <Line previous='1' current='2'/>
 
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -106,6 +133,16 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+  },
+  line: {
+    flexDirection: 'row',
+    marginTop: 12,
+    paddingHorizontal: 24,
+
+    
+    alignContent: 'stretch',
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
   },
   sectionTitle: {
     fontSize: 24,
